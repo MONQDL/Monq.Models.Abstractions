9548536b4773c73af6402106f20dadd33bd13321
NorthRebel <zatinasever@gmail.com>
2020-07-24 11:20:45 +0300

    ⚡️ Реализована принимаемая модель для обновления поля строкового типа с заданным поведением (Override/Append). - 🔨 Реализован хелпер для обновления значения поля модели строкового типа. - ✅ Реализованы тесты по новой модели. - ➕ Newtonsoft.Json v12.0.3. - 🔖 v3.4

---
+++ src/Monq.Models.Abstractions/ModelPropertyPutViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Monq.Models.Abstractions
+{
+    /// <summary>
+    /// Принимаемая модель представления для обновления свойства сущности.
+    /// </summary>
+    public class ModelPropertyPutViewModel
+    {
+        /// <summary>
+        /// Новое значение
+        /// </summary>
+        public string Value { get; set; } = null;
+
+        /// <summary>
+        /// Поведение при обновлении свойства сущности.
+        /// </summary>
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ModelPropertyPutBehavior Behavior { get; set; } = ModelPropertyPutBehavior.Override;
+    }
+}
